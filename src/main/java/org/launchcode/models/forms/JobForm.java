65900f28452b27e67255de0bdd00897eee4f0d5d
marypeterson <the.airforce.captain@gmail.com>
2018-06-25 18:57:29 -0500

    other job fields to create job

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -42,6 +42,10 @@ public class JobForm {
         */
 
         employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
+
 
     }
 
